372d012e2f4b1d646f9eaa58b99982e568cf4792
mcleshiy16 <168570280+mcleshiy16@users.noreply.github.com>
2024-04-30 22:55:18 +0700

    Add files via upload

---
+++ api/v1/database/access.py
@@ -0,0 +1,6 @@
+def GetConnectionString():
+    return "postgresql://postgres:postgres@localhost:5432/DigitalUp_Task3_DB"
+
+
+def GetConnectionString_render():
+    return "postgresql://postgres_user:tU660hLJF9bQh0QhixERY6ojPpRCJt4P@dpg-coogj30l5elc739p1ceg-a/digitalup_fastapi_task3_db"
